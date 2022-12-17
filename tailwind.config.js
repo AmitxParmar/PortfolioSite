7811998191b5bedf4670d2b7b8b2b7d5506f6638
AmitxParmar <amitparmar901@gmail.com>
2022-12-17 17:07:22 +0530

    done with header and animation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{jsx,js,tsx,ts}',
+    './components/**/*.{ts,tsx,jsx,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
