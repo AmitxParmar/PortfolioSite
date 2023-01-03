f9511451ea789e2e58e218bcc78617cc4d1bc4e8
AmitxParmar <amitparmar901@gmail.com>
2023-01-04 00:01:24 +0530

    did Skills, doing Projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,52 @@
-/* eslint-disable react/no-unescaped-entities */
-import About from 'components/About';
-import Header from 'components/Header';
-import Hero from 'components/Hero';
-import Skills from 'components/Skills';
-import WorkExperience from 'components/WorkExperience';
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+import Link from 'next/link';
 
+import About from '../components/About';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Skills from '../components/Skills';
+import WorkExperience from '../components/WorkExperience';
+
+/* import ContactMe from '../components/ContactMe'; */
+/* import Projects from '../components/Projects'; */
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
-
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80">
       <Head>
-        <title>Amit&apos;s Portfolio</title>
+        <title>Sonny&apos;s Portfolio</title>
       </Head>
-
       <Header />
-
-      <section id='hero' className='snap-start'>
+      <section className="snap-start" id="hero">
         <Hero />
       </section>
-
-      <section id='about' className='snap-center'>
+      <section className="snap-center" id="about">
         <About />
       </section>
-
-      <section id='experience' className='snap-center'>
+      <section className="snap-center" id="experience">
         <WorkExperience />
       </section>
-
-      <section id='skills' className='snap-center'>
+      <section className="snap-start" id="skills">
         <Skills />
       </section>
+      {/* <section className="snap-start" id="projects">
+        <Projects />
+      </section>
+      <section className="snap-start" id="contact">
+        <ContactMe />
+      </section> */}
 
-
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="w-10 h-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="https://i.imgur.com/e2yvD6A.png"
+              alt="Image"
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 }
